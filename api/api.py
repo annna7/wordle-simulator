557e6080024cd0b82a1f6f0237793eb267386189
anna <annapecheanu1@gmail.com>
2023-08-13 11:35:49 +0300

    Fixed start-api script in package.json

--- api/api.py
+++ api/api.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the parent directory of src to the path
+current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+sys.path.append(current_dir)
+
 from src.app import app
 
 if __name__ == '__main__':
